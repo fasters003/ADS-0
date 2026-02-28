01b2315b6d2290060043e136dff23c4fa2f7e104
fasters003 <andrey1990ver@gmail.com>
2026-02-28 11:25:25 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,13 +1,14 @@
-	// Copyright 2021 NNTU-CS
+// Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-	if (a < 0) a = -a;
-if (b < 0) b = -b;
-
-while (b != 0) {
-    int temp = b;
-    b = a % b;
-    a = temp;
-}
-	return a;
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+	
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    
+    return a;
 }
